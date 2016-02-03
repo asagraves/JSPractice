f7734d0fb955026e148d1c94376aec7c8569fc88
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-03 09:25:09 -0600

    pracitice

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-var a;
+var person = new Object();
 
-//goes out to internet and looks for value
-a = "hi";
-if (a) {
-  console.log('something is there');
-}
+person['firstname'] = "andrew";
+person['lastname'] = "sagraves";
+person['street'] = "8149 pepperdine way";
+
+var firstNameProperty = "firstname";
+var street = "street";
+console.log(person);
+
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+
+person.address = new Object();
+person.address.street = "8149 pepperdine way";
+person.address.city = "knoxville";
+person.address.state = "tn";
+
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
+
+
+
