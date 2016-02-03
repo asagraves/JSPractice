e2e2d574f1b8f7d57580dc36ee61bb02d52a3d03
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-03 13:57:32 -0600

    name value pairs

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-var person = new Object();
+var Andrew = {
+  firstname: 'Andrew',
+  lastname: 'sagraves',
+  address: {
+    street: '8149 pepperdine way',
+    city: 'knoxville'
+  }
 
-person['firstname'] = "andrew";
-person['lastname'] = "sagraves";
-person['street'] = "8149 pepperdine way";
+};
 
-var firstNameProperty = "firstname";
-var street = "street";
-console.log(person);
+function greet(person) {
+  console.log('Hello ' + person.firstname)
+}
+
+greet(Andrew);
+
+greet({
+  firstname: 'Andrew',
+  lastname: 'sagraves'
+});
+
+Andrew.address2 = {
+  street: 'bloom dr'
+}
 
 
-console.log(person.firstname);
-console.log(person.lastname);
-
-
-person.address = new Object();
-person.address.street = "8149 pepperdine way";
-person.address.city = "knoxville";
-person.address.state = "tn";
-
-
-console.log(person.address.street);
-console.log(person.address.city);
-console.log(person["address"]["state"]);
 
 
 
